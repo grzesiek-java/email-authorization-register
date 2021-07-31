b6b1eea2011f267d00402ecd2f243d5aae79b2bc
szadoq <kweniek@gmail.com>
2021-07-31 14:13:54 +0200

    add wired appuser to db config db

--- src/main/java/pl/szadoq/demo/config/WebSecurityConfig.java
+++ src/main/java/pl/szadoq/demo/config/WebSecurityConfig.java
@@ -4,13 +4,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import pl.szadoq.demo.service.UserDetailsServiceImpl;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+
+    private UserDetailsServiceImpl userDetailsService;
+
+    public WebSecurityConfig(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.userDetailsService(userDetailsService);
     }
 
     @Override
